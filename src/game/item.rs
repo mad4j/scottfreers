badd11f0046543c21a913c9a67483d8dff979803
Daniele Olmisani <daniele.olmisani@gmail.com>
2023-01-08 10:16:33 +0100

    early usage of regex crate

--- src/game/item.rs
+++ src/game/item.rs
@@ -1,17 +1,19 @@
 use std::{
     fmt,
     fs::File,
-    io::{BufRead, BufReader, Error},
+    io::{BufRead, BufReader, Error, ErrorKind},
 };
 
-use super::{parse};
+use regex::Regex;
+
+use super::parse;
 
 #[derive(Debug)]
 pub struct Item {
     text: String,
+    auto_get: String,
     location: u8,
     initial_location: u8,
-    auto_get: String,
 }
 
 impl parse::Parse for Item {
@@ -23,10 +25,20 @@ impl parse::Parse for Item {
 
         r.read_line(&mut line)?;
 
+        let re = Regex::new(r#"^"(.*?)"\s*(\d{1,3})\s*$"#)
+            .map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+        println!("'{:?}' - {}", line, re.is_match(&line));
+
+        let cap = re.captures(&line).unwrap();
+        println!("'{}' - {}", &cap[1], &cap[2]);
+
+        let loc =
+            u8::from_str_radix(&cap[2], 10).map_err(|e| Error::new(ErrorKind::InvalidData, e))?;
+
         let item = Item {
-            text: line,
-            location: 0,
-            initial_location: 0,
+            text: String::from(&cap[1]),
+            location: loc,
+            initial_location: loc,
             auto_get: String::new(),
         };
 
@@ -38,7 +50,7 @@ impl fmt::Display for Item {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "Item {{ text: \"{:?}\", location: {}, initial_location: {}, auto_get: \"{:?}\" }}",
+            "Item {{ text: \"{}\", location: {}, initial_location: {}, auto_get: \"{}\" }}",
             self.text, self.location, self.initial_location, self.auto_get
         )
     }
